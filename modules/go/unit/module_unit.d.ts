98715cf83e676b7ff5be9f9261bb0dbe051d5c54
Zen.Liu <lcz20@163.com>
2024-05-20 21:23:17 +0800

    add go/unit for backward compat

---
+++ modules/go/unit/module_unit.d.ts
@@ -0,0 +1,326 @@
+
+
+declare module "go/unit" {
+
+
+    /**
+     * root directory of executable
+     */
+    export const root: string
+    /**
+     * extension of executable
+     */
+    export const ext: string
+    /**
+     * executable name with extension
+     */
+    export const executable: string
+    /**
+     * name of executable without extension
+     */
+    export const name: string
+    export const pathSeparator: string
+    export const pathListSeparator: string
+
+
+    /**
+     * Expand a relative path to absolute path.
+     * first '@' expand to root
+     * '$NAME or %NAME%' expand as environment variable.
+     * @param path path value
+     */
+    export function expand(path: string): string
+
+    /**
+     * prepend value to environment variable
+     * @param name variable name
+     * @param values values
+     */
+    export function pre(name: string, ...values: string[])
+
+    /**
+     * append value to environment variable
+     * @param name variable name
+     * @param values values
+     */
+    export function ap(name: string, ...values: string[])
+
+    /**
+     * prepend path to environment variable
+     * @see os.expand
+     * @param name variable name
+     * @param paths values
+     */
+    export function prep(name: string, ...paths: string[])
+
+    /**
+     * append path to environment variable
+     * @see os.expand
+     * @param name variable name
+     * @param paths values
+     */
+    export function app(name: string, ...paths: string[])
+
+    export function set(name: string, ...values: string[])
+
+    /**
+     * set path to environment variable, override exists one
+     * @see os.expand
+     * @param name variable name
+     * @param paths values
+     */
+    export function setp(name: string, ...paths: string[])
+
+    /**
+     * put value to environment variable, not override exists one
+     * @param name variable name
+     * @param values
+     */
+    export function put(name: string, ...values: string[])
+
+    /**
+     * put path to environment variable, not override exists one
+     * @see os.expand
+     * @param name variable name
+     * @param paths values
+     */
+    export function putp(name: string, ...paths: string[])
+
+    /**
+     * get environment variable
+     * @param name the name
+     * @returns empty string if not exists one
+     */
+    export function variable(name: string): string
+
+    /**
+     * eval script file
+     * @see os.expand
+     * @param path of typescript file or javascript file
+     */
+    export function evalFile(path: string): any
+
+    /**
+     * eval script files
+     * @see os.expand
+     * @param paths of typescript files or javascript files
+     */
+    export function evalFiles(...paths: string[]): any[]
+
+
+
+    export function mkdir(path: string)
+
+    export function mkdirAll(path: string)
+
+    export function exists(path: string): boolean
+
+    export function write(path: string, data: Uint8Array)
+
+    export function writeText(path: string, data: string)
+
+    export function read(path: string): Uint8Array
+
+    export function readText(path: string): string
+
+    export function chdir(path: string)
+
+    export function pwd(): string
+    export function mkdirAll(path: string, perm: number)//511=0777
+
+    export function rename(path, newPath: string)
+    export function userCacheDir(): string
+
+    export function userConfigDir(): string
+
+    export function userHomeDir(): string
+    export function chown(path: string, uid, gid: number)
+    export function getUID(): number
+
+    export function getGID(): number
+
+    export function getPagesize(): number
+
+    export function getPid(): number
+
+    export function remove(name: string)
+
+    export function removeAll(name: string)
+
+    export function hostname(): string
+
+    export function tempDir(): string
+    export function ls(path?: string): Array<{
+        dir: boolean, name: string, mode: string,
+        size: number, modified: string }>
+
+    export function stat(path: string): undefined | {
+        name(): string
+
+        size(): number
+
+        mode(): number
+
+        modTime(): any /*golang/time.Time*/
+
+        isDir(): boolean
+    }
+
+    export interface ExecOption extends ProcOption {
+        /**
+         * sleep after execute, when with use await, sleep after execute finished.
+         */
+        sleep?: string
+        /**
+         * optional execute
+         */
+        optional?: boolean
+        /**
+         * await execute finish
+         */
+        await?: boolean
+
+    }
+
+    export interface ProcOption {
+        /**
+         * command, can be empty when use with sleep to just do a sleep.
+         */
+        cmd: string
+        /**
+         * arguments
+         */
+        args?: string[]
+        /**
+         * customer working directory
+         */
+        workingDir?: string
+        /**
+         * show window (only support windows)
+         */
+        showWindow?: boolean
+        /**
+         * expand paths in args
+         */
+        pathPath?: boolean
+
+
+    }
+
+    export interface SubProc {
+        /**
+         * start and not wait process exit
+         * @returns error or empty string
+         */
+        start(): string
+
+        /**
+         * wait started process exit
+         * @returns error or empty string
+         */
+        wait(): string
+
+        /**
+         * release process resources, when not use wait()
+         * free() also called
+         * @returns error or empty string
+         */
+        release(): string
+
+        /**
+         * run process and wait for exit
+         * @returns error or empty string
+         */
+        run(): string
+
+        /**
+         * run and fetch process output
+         * @returns  console encoding binary data and error string
+         */
+        output(): { data: Uint8Array, error: string }
+
+        /**
+         * run and fetch process output include stdError
+         * @returns  console encoding binary data and error string
+         */
+        combinedOutput(): { data: Uint8Array, error: string }
+
+        /**
+         * use with start|run method to read from stdout
+         * ,call once before invoke  start|run
+         * @returns  console encoding binary data and error string
+         */
+        readStdout(): { data: Uint8Array, error: string }
+
+        /**
+         * use with start|run method to read from stderr
+         * ,call once before invoke  start|run
+         * @returns  console encoding binary data and error string
+         */
+        readStderr(): { data: Uint8Array, error: string }
+
+        /**
+         * use with start|run method to write to stdin
+         * ,call once with null before invoke  start|run
+         * @returns  written bytes count or error string
+         */
+        writeStdin(data: Uint8Array | null): { write: number, error: string }
+
+        /**
+         * false if not stated yet
+         */
+        exited(): boolean
+
+        /**
+         * false if not stated yet
+         */
+        success(): boolean
+
+        /**
+         * -1 if not stated yet
+         */
+        sysTime(): number
+
+        /**
+         * -1 if not stated yet
+         */
+        userTime(): number
+
+        /**
+         * -1 if not stated yet
+         */
+        sysNanoTime(): number
+
+        /**
+         * -1 if not stated yet
+         */
+        userNanoTime(): number
+
+        /**
+         * kill the process
+         * @returns error or empty string
+         */
+        kill(): string
+
+        /**
+         * free all used pipes for stdout stderr and stdin
+         */
+        free()
+
+        /**
+         * convert local console encoding buf data to utf8 encode data
+         * @param buf
+         */
+        fromConsole(buf: Uint8Array): Uint8Array
+
+        /**
+         * convert utf8 buf data to local console encoding
+         * @param buf
+         */
+        toConsole(buf: Uint8Array): Uint8Array
+    }
+
+    export function exec(option: ExecOption)
+
+    export function proc(option: ProcOption): SubProc
+}
---
+++ modules/go/unit/unit.go
@@ -0,0 +1,356 @@
+package unit
+
+import (
+	_ "embed"
+	"fmt"
+	"github.com/ZenLiuCN/engine"
+	"github.com/ZenLiuCN/fn"
+	"io"
+	"os"
+)
+
+var (
+	//go:embed module_unit.d.ts
+	UnitDefine []byte
+	UnitMap    = map[string]any{
+		"root":              engine.GetRoot(),
+		"simpleName":        engine.GetName(),
+		"ext":               engine.GetExt(),
+		"executable":        engine.GetExecutable(),
+		"pathSeparator":     engine.GetPathSeparator(),
+		"pathListSeparator": engine.GetPathListSeparator(),
+		"expand":            engine.EnvExpand,
+		"pre":               engine.EnvPrepend,
+		"prep":              engine.EnvPrependPath,
+		"ap":                engine.EnvAppend,
+		"app":               engine.EnvAppendPath,
+		"set":               engine.EnvSet,
+		"setp":              engine.EnvSetPath,
+		"put":               engine.EnvPut,
+		"putp":              engine.EnvPutPath,
+		"variable":          engine.EnvVar,
+		"mkdir":             engine.Mkdir,
+		"mkdirAll":          engine.MkdirAll,
+		"exists":            engine.FileExists,
+		"write":             engine.WriteBinaryFile,
+		"writeText":         engine.WriteTextFile,
+		"read":              engine.ReadBinaryFile,
+		"readText":          engine.ReadTextFile,
+		"chdir":             engine.Chdir,
+		"pwd":               engine.Pwd,
+		"getGID":            os.Geteuid,
+		"getUID":            os.Geteuid,
+		"getPagesize":       os.Getpagesize,
+		"getPid":            os.Getpid,
+		"userCacheDir":      os.UserCacheDir,
+		"userConfigDir":     os.UserConfigDir,
+		"userHomeDir":       os.UserHomeDir,
+		"hostname":          os.Hostname,
+		"tempDir":           os.TempDir,
+		"mkdirALL": func(path string, perm os.FileMode) error {
+			return os.MkdirAll(engine.EnvExpand(path), perm)
+		},
+		"rename": func(path, new string) error {
+			return os.Rename(engine.EnvExpand(path), engine.EnvExpand(new))
+		},
+		"remove": func(path string) error {
+			return os.Remove(engine.EnvExpand(path))
+		},
+		"removeAll": func(path string) error {
+			return os.RemoveAll(engine.EnvExpand(path))
+		},
+		"chown": func(path string, uid, gid int) error {
+			return os.Chown(engine.EnvExpand(path), uid, gid)
+		},
+		"chmod": func(path string, mod os.FileMode) error {
+			return os.Chmod(engine.EnvExpand(path), mod)
+		},
+		"ls": func(path string) (r []map[string]any, err error) {
+			defer func() {
+				if r := recover(); r != nil {
+					switch v := r.(type) {
+					case error:
+						err = v
+					default:
+						err = fmt.Errorf("%s", v)
+					}
+				}
+			}()
+			if path == "" {
+				path, _ = engine.Pwd()
+			}
+			f := fn.Panic1(os.Open(engine.EnvExpand(path)))
+			defer fn.IgnoreClose(f)
+			dir := fn.Panic1(f.ReadDir(0))
+			for _, entry := range dir {
+				info := fn.Panic1(entry.Info())
+				r = append(r, map[string]any{
+					"dir":      entry.IsDir(),
+					"name":     entry.Name(),
+					"mode":     entry.Type().String(),
+					"size":     info.Size(),
+					"modified": info.ModTime().Format("2006-01-02 15:04:05.000"),
+				})
+			}
+			return
+		},
+
+		"stat": engine.Stat,
+		"exec": engine.Execute,
+		"proc": func(option *engine.ProcOption) (r *SubProc, err error) {
+			s, err := engine.OpenProc(option)
+			if err != nil {
+				return nil, err
+			}
+			return &SubProc{s: s}, nil
+		},
+	}
+)
+
+func init() {
+	engine.RegisterModule(Unit{})
+}
+
+type Unit struct {
+}
+
+func (o Unit) Identity() string {
+	return "go/unit"
+}
+func (o Unit) TypeDefine() []byte {
+	return UnitDefine
+}
+func (o Unit) Exports() map[string]any {
+	return UnitMap
+}
+
+type SubProc struct {
+	s   *engine.SubProcess
+	out io.ReadCloser
+	err io.ReadCloser
+	in  io.WriteCloser
+	buf []byte
+}
+
+func (s SubProc) FromConsole(data []byte) []byte {
+	buf := engine.GetBytesBuffer()
+	defer engine.PutBytesBuffer(buf)
+	defer buf.Reset()
+	buf.Write(data)
+	fn.Panic(engine.FromNativeConsole(buf))
+	return buf.Bytes()
+}
+func (s SubProc) ToConsole(data []byte) []byte {
+	buf := engine.GetBytesBuffer()
+	defer engine.PutBytesBuffer(buf)
+	defer buf.Reset()
+	buf.Write(data)
+	fn.Panic(engine.ToNativeConsole(buf))
+	return buf.Bytes()
+}
+func (s SubProc) Run() string {
+	err := s.s.Run()
+	if err != nil {
+		return err.Error()
+	}
+	return ""
+}
+func (s SubProc) Wait() string {
+	err := s.s.Wait()
+	if err != nil {
+		return err.Error()
+	}
+	return ""
+}
+func (s SubProc) ReadStdout() BinaryError {
+	var e error
+	if s.out == nil {
+		s.out, e = s.s.StderrPipe()
+		if e != nil {
+			return BinaryError{
+				Data:  nil,
+				Error: e.Error(),
+			}
+		}
+	}
+	if s.buf == nil {
+		s.buf = make([]byte, 512)
+	}
+	n, e := s.out.Read(s.buf)
+	if e != nil {
+		return BinaryError{
+			Data:  nil,
+			Error: e.Error(),
+		}
+	}
+	return BinaryError{
+		Data:  s.buf[:n],
+		Error: "",
+	}
+}
+func (s SubProc) ReadStderr() BinaryError {
+	var e error
+	if s.err == nil {
+		s.err, e = s.s.StderrPipe()
+		if e != nil {
+			return BinaryError{
+				Data:  nil,
+				Error: e.Error(),
+			}
+		}
+	}
+	if s.buf == nil {
+		s.buf = make([]byte, 512)
+	}
+	n, e := s.err.Read(s.buf)
+	if e != nil {
+		return BinaryError{
+			Data:  nil,
+			Error: e.Error(),
+		}
+	}
+	return BinaryError{
+		Data:  s.buf[:n],
+		Error: "",
+	}
+}
+func (s SubProc) WriteStdin(data []byte) WriteError {
+	var e error
+	if s.in == nil {
+		s.in, e = s.s.StdinPipe()
+		if e != nil {
+			return WriteError{
+				Write: 0,
+				Error: e.Error(),
+			}
+		}
+	}
+	if data == nil {
+		return WriteError{
+			Write: 0,
+			Error: "",
+		}
+	}
+	if s.buf == nil {
+		s.buf = make([]byte, 512)
+	}
+	n, e := s.in.Write(data)
+	if e != nil {
+		return WriteError{
+			Write: 0,
+			Error: e.Error(),
+		}
+	}
+	return WriteError{
+		Write: n,
+		Error: "",
+	}
+}
+func (s SubProc) Start() string {
+	err := s.s.Start()
+	if err != nil {
+		return err.Error()
+	}
+	return ""
+}
+func (s SubProc) Exited() bool {
+	if s.s.ProcessState == nil {
+		return false
+	}
+	return s.s.ProcessState.Exited()
+}
+func (s SubProc) Success() bool {
+	if s.s.ProcessState == nil {
+		return false
+	}
+	return s.s.ProcessState.Exited()
+}
+func (s SubProc) SysTime() int64 {
+	if s.s.ProcessState == nil {
+		return -1
+	}
+	return s.s.ProcessState.SystemTime().Milliseconds()
+}
+func (s SubProc) UserTime() int64 {
+	if s.s.ProcessState == nil {
+		return -1
+	}
+	return s.s.ProcessState.UserTime().Milliseconds()
+}
+func (s SubProc) SysNanoTime() int64 {
+	if s.s.ProcessState == nil {
+		return -1
+	}
+	return s.s.ProcessState.SystemTime().Nanoseconds()
+}
+func (s SubProc) UserNanoTime() int64 {
+	if s.s.ProcessState == nil {
+		return -1
+	}
+	return s.s.ProcessState.UserTime().Nanoseconds()
+}
+func (s SubProc) Kill() string {
+	err := s.s.Process.Kill()
+	if err != nil {
+		return err.Error()
+	}
+	return ""
+}
+func (s SubProc) Release() string {
+	s.Free()
+	err := s.s.Process.Release()
+	if err != nil {
+		return err.Error()
+	}
+	return ""
+}
+func (s SubProc) Output() BinaryError {
+	b, e := s.s.Output()
+	if e != nil {
+		return BinaryError{
+			Data:  nil,
+			Error: e.Error(),
+		}
+	}
+	return BinaryError{
+		Data:  b,
+		Error: "",
+	}
+}
+func (s SubProc) CombinedOutput() BinaryError {
+	b, e := s.s.CombinedOutput()
+	if e != nil {
+		return BinaryError{
+			Data:  nil,
+			Error: e.Error(),
+		}
+	}
+	return BinaryError{
+		Data:  b,
+		Error: "",
+	}
+}
+func (s SubProc) Free() {
+	if s.out != nil {
+		_ = s.out.Close()
+		s.out = nil
+	}
+	if s.in != nil {
+		_ = s.in.Close()
+		s.in = nil
+	}
+	if s.err != nil {
+		_ = s.err.Close()
+		s.err = nil
+	}
+	s.buf = nil
+}
+
+type BinaryError struct {
+	Data  []byte
+	Error string
+}
+type WriteError struct {
+	Write int
+	Error string
+}
