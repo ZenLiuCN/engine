27753d31a64235b0e39de0528610c68093630765
Zen.Liu <lcz20@163.com>
2024-05-30 10:45:40 +0800

    add version info for cmd

--- cmd/build_linux.sh
+++ cmd/build_linux.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-CGO_LDFLAGS="-L.libs/linux/" go build -tags=duckdb_use_lib,chrome,ducdb,gse,all -o engine -ldflags="-X main.Version=$(git describe) -s -w" .
+CGO_LDFLAGS="-L.libs/linux/" go build -tags=duckdb_use_lib,chrome,ducdb,gse,all -o engine -ldflags="-X main.Version=$(git describe --abbrev=0 --tags) -s -w" .
--- cmd/build_win.sh
+++ cmd/build_win.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-CGO_LDFLAGS="-L.libs/win/"  go build -tags=duckdb_use_lib,all -o engine.exe -ldflags="-X main.Version=$(git describe) -s -w" . && \
+CGO_LDFLAGS="-L.libs/win/"  go build -tags=duckdb_use_lib,all -o engine.exe -ldflags="-X main.Version=$(Git describe --abbrev=0 --tags) -s -w" . && \
 upx --lzma engine.exe
