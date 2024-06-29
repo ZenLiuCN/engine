fa552f9bea902ede9b27e5ca358c1dc3977b2a62
Zen.Liu <lcz20@163.com>
2024-06-29 13:15:14 +0800

    sync engine

--- cmd/build_win.sh
+++ cmd/build_win.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-CGO_LDFLAGS="-L.libs/win/"  go build -tags=duckdb_use_lib,all -o engine.exe -ldflags="-X main.Version=$(Git describe --abbrev=0 --tags) -s -w" . && \
+CGO_LDFLAGS="-L$(pwd)/../../Loader/.libs/win/" \
+go build -tags=duckdb_use_lib,all -o engine.exe \
+-ldflags="-X main.Version=$(Git describe --abbrev=0 --tags) -s -w" . && \
 upx --lzma engine.exe
